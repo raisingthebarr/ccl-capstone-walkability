953e02dce8edf3dab42f42171a2660ffe9bf0fc7
anumitaj <aj57@rice.edu>
2021-02-28 18:01:04 -0600

    new changes 2/28

--- Rscript/Loading Data.R
+++ Rscript/Loading Data.R
@@ -2,6 +2,7 @@
 
 rm(list = ls())
 
+#loading packages-----
 library(rlang)
 library(tidyverse)
 library(janitor)
@@ -17,8 +18,10 @@ library(sp)
 library(dplyr)
 library(maptools)
 
+#setting working directory
 setwd("C:/Users/Anumita Jain/Desktop/Junior Year/ccl-capstone-walkability/Data")
 
+#loading data-----
 sidewalks <- st_read("sidewalks", "sidewalks")
 lrtstations <- st_read("lrtstations", "lrtstation")
 lrtlines <- st_read("lrtlines", "lrtlines")
@@ -27,6 +30,7 @@ zones <- st_read("zones", "ElementaryAttendanceZones1920")
 busstops <- st_read("busstops", "busstops")
 busroutes <- st_read("busroutes", "busroutes")
 
+#cleaning data-----
 clean_shape <- function(.data, .crs=nad83) {
   nad83<-st_crs("+proj=longlat +ellps=WGS84 +datum=NAD83 +no_defs +towgs84=0,0,0")
   
@@ -37,16 +41,44 @@ clean_shape <- function(.data, .crs=nad83) {
   return(data)
 }
 
-roberts <- subset(zones, OBJECTID_1 == 152)
+clean_shape(sidewalks)
+clean_shape(lrtstations)
+clean_shape(lrtlines)
+clean_shape(bikeways)
+clean_shape(zones)
+clean_shape(busstops)
+clean_shape(busroutes)
 
-bounds <- extent(roberts)
-lon_min <- as.numeric(bounds@xmin)
-lon_max <- as.numeric(bounds@xmax)
-lat_min <- as.numeric(bounds@ymin)
-lat_max <- as.numeric(bounds@ymax)
+#notes on shapes-----
+#sidewalks = multilinestring
+#lrtstations = point
+#lrtlines = linestring
+#bikeways = multilinestring
+#busstops = point
+#busroutes = multilinestring
+#zones = polygon
 
-bbox <- st_bbox(c(xmin = lon_min, xmax = lon_max, 
-                  ymax = lat_max, ymin = lat_min), crs = st_crs(roberts))
+#calculating percentage -----
 
-robertslrt <- st_intersects(lrtstations, bbox)
-robertslrt1 <- st_intersects(lrtstations, roberts)
+zoneslrts <- st_join(lrtstations, zones)
+rankinglrts <- tabyl(zoneslrts, Elementary)
+
+zoneslrtl <- st_intersection(lrtlines,zones)
+rankinglrtl <- tapply(st_length(zoneslrtl), zoneslrtl$Elementary,sum)
+View(rankinglrtl)
+
+zonesbus <- st_join(busstops, zones)
+rankingbus <- tabyl(zonesbus, Elementary)
+
+zonesbusr <- st_intersection(busroutes, zones)
+rankingbusr <- tapply(st_length(zonesbusr), zonesbusr$Elementary, sum)
+View(rankingbusr)
+
+zonesbike <- st_join(bikeways, zones)
+rankingbike <- tapply(st_length(zonesbike), zonesbike$Elementary, sum)
+View(rankingbike)
+
+zonessw <- st_join(sidewalks,zones)
+rankingsidewalk <- tapply(st_length(zonessw), zonessw$Elementary,sum)
+View(rankingsidewalk)
+
