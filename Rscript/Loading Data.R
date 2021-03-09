33c519bea8b4ce41c2aad3a3ca907b3966afb0e0
anumitaj <aj57@rice.edu>
2021-03-08 23:24:06 -0600

    3/8

--- Rscript/Loading Data.R
+++ Rscript/Loading Data.R
@@ -27,13 +27,13 @@ sidewalks <- st_read("sidewalks", "sidewalks")
 lrtstations <- st_read("lrtstations", "lrtstation")
 lrtlines <- st_read("lrtlines", "lrtlines")
 bikeways <- st_read("bikeways", "bikeways")
-zones <- st_read("zones", "ElementaryAttendanceZones1920")
 busstops <- st_read("busstops", "busstops")
 busroutes <- st_read("busroutes", "busroutes")
+zones <- st_read("zones", "ElementaryAttendanceZones1920")
+zones <- zones[order(zones$Elementary),]
 
 schools <- st_read("schools", "schools")
 schools <- subset(schools, DistName == "HOUSTON ISD")
-
 
 #cleaning data-----
 clean_shape <- function(.data, .crs=nad83) {
@@ -70,22 +70,50 @@ clean_shape(schools)
 
 zoneslrts <- st_join(lrtstations, zones)
 rankinglrts <- tabyl(zoneslrts, Elementary)
+rankinglrts$area <- zones$Shape_Area
+rankinglrts <- transform(rankinglrts, percent = n / area)
 
 zoneslrtl <- st_intersection(lrtlines,zones)
 rankinglrtl <- tapply(st_length(zoneslrtl), zoneslrtl$Elementary,sum)
+rankinglrtl <- as.data.frame(rankinglrtl)
+rankinglrtl[is.na(rankinglrtl)] <- "0"
+names(rankinglrtl)[names(rankinglrtl) == "rankinglrtl"] <- "length"
+rankinglrtl <- data.frame(rankinglrtl$length, zones$Elementary, zones$Shape_Area)
+rankinglrtl$rankinglrtl.length <- as.numeric(rankinglrtl$rankinglrtl.length)
+rankinglrtl <- transform(rankinglrtl, percent = rankinglrtl.length / zones.Shape_Area)
 
 zonesbus <- st_join(busstops, zones)
 rankingbus <- tabyl(zonesbus, Elementary)
+rankingbus <- na.omit(rankingbus)
+rankingbus$area <- zones$Shape_Area
+rankingbus <- transform(rankingbus, percentbus = n / area)
 
 zonesbusr <- st_intersection(busroutes, zones)
 rankingbusr <- tapply(st_length(zonesbusr), zonesbusr$Elementary, sum)
+rankingbusr <- as.data.frame(rankingbusr)
+rankingbusr[is.na(rankingbusr)] <- "0"
+names(rankingbusr)[names(rankingbusr) == "rankingbusr"] <- "length"
+rankingbusr <- data.frame(rankingbusr$length, zones$Elementary, zones$Shape_Area)
+rankingbusr$rankingbusr.length <- as.numeric(rankingbusr$rankingbusr.length)
+rankingbusr <- transform(rankingbusr, percent = rankingbusr.length / zones.Shape_Area)
 
 zonesbike <- st_join(bikeways, zones)
 rankingbike <- tapply(st_length(zonesbike), zonesbike$Elementary, sum)
+rankingbike <- as.data.frame(rankingbike)
+rankingbike[is.na(rankingbike)] <- "0"
+names(rankingbike)[names(rankingbike) == "rankingbike"] <- "length"
+rankingbike <- data.frame(rankingbike$length, zones$Elementary, zones$Shape_Area)
+rankingbike$rankingbike.length <- as.numeric(rankingbike$rankingbike.length)
+rankingbike <- transform(rankingbike, percent = rankingbike.length / zones.Shape_Area)
 
 zonessw <- st_join(sidewalks,zones)
-rankingsidewalk <- tapply(st_length(zonessw), zonessw$Elementary,sum)
-
+rankingsw <- tapply(st_length(zonessw), zonessw$Elementary,sum)
+rankingsw <- as.data.frame(rankingsw)
+rankingsw[is.na(rankingsw)] <- "0"
+names(rankingsw)[names(rankingsw) == "rankingsw"] <- "length"
+rankingsw <- data.frame(rankingsw$length, zones$Elementary, zones$Shape_Area)
+rankingsw$rankingsw.length <- as.numeric(rankingsw$rankingsw.length)
+rankingsw <- transform(rankingsw, percent = rankingsw.length / zones.Shape_Area)
 
 
 #trying to create 2-mile isochrone map-----
