d9745123bfc0b4752160776422ae8d15e57a0801
anumitaj <aj57@rice.edu>
2021-03-23 00:33:48 -0500

    3/23

--- Rscript/Loading Data.R
+++ Rscript/Loading Data.R
@@ -32,9 +32,10 @@ busstops <- st_read("busstops", "busstops")
 busroutes <- st_read("busroutes", "busroutes")
 zones <- st_read("zones", "ElementaryAttendanceZones1920")
 zones <- zones[order(zones$Elementary),]
-
+load("C:/Users/Anumita Jain/Downloads/hcad-parcels.RData")
 schools <- st_read("schools", "schools")
 schools <- subset(schools, DistName == "HOUSTON ISD")
+
 
 #cleaning data-----
 clean_shape <- function(.data, .crs=nad83) {
@@ -55,105 +56,44 @@ clean_shape(zones)
 clean_shape(busstops)
 clean_shape(busroutes)
 clean_shape(schools)
+parcels <- clean_shape(parcels_full)
+zones <- clean_shape(zones)
 
 
-#notes on shapes-----
-#sidewalks = multilinestring
-#lrtstations = point
-#lrtlines = linestring
-#bikeways = multilinestring
-#busstops = point
-#busroutes = multilinestring
-#zones = polygon
+#intersecting parcels with zones----
+parcels_full$geom2 <- st_centroid(st_geometry(parcels_full))
+View(parcels_full$geom2, parcels_full$geom)
+
+rightcrs <- st_crs(zones)
+st_transform(parcels_full, crs = st_crs(zones))
+st_transform(zones, crs = st_crs(parcels_full))
+parcels_full$geom <- st_cast(parcels_full$geometry, "POINT")
+
+
+clean_shape(parcels_full)
+
+parcels_full %>% st_set_crs(4326) %>% st_transform(NAD83)
+
+parcels_full <- st_as_sf(parcels_full)
+
+zoneparcel <- st_join(zones, parcels_full)
+st_crs(parcels_full)
+st_crs(zones)
 
 #experimenting with opentripplanner-----
-library(rJava)
 library(opentripplanner)
 
 
 otp_check_java()
 
+library(opentripplanner)
 path_data <- file.path("C:/Users/Anumita Jain/Desktop/Junior Year/ccl-capstone-walkability/Data", "OTP")
-dir.create(path_data)
-path_otp <- otp_dl_jar(path_data, cache = FALSE)
-log1 <- otp_build_graph(otp = path_otp , dir = path_data) 
+path_otp <- otp_dl_jar()
+
+log1 <- otp_build_graph(otp=path_otp, dir = path_data)
 log2 <- otp_setup(otp = path_otp, dir = path_data)
 otpcon <- otp_connect()
-otp_plan(otp_connect, fromPlace = c(29.722388419368436, -95.40204852710573),
-         toPlace = c(29.727476254276088, -95.40782504533031))
-
-
-path_data <- file.path("C:/Users/Anumita Jain/Desktop/Junior Year/ccl-capstone-walkability/Data", "OTP")
-dir.create(path_data) 
-path_otp <- otp_dl_jar(path_data, cache = FALSE)
-log1 <- otp_build_graph(otp = path_otp, dir = path_data) 
-log2 <- otp_setup(otp = path_otp, dir = path_data)
-otpcon <- otp_connect()
-route <- otp_plan(otpcon, 
-                  fromPlace = c(), 
-                  toPlace = c())
-
-#calculating percentage -----
-
-zoneslrts <- st_join(lrtstations, zones)
-rankinglrts <- tabyl(zoneslrts, Elementary)
-rankinglrts$area <- zones$Shape_Area
-rankinglrts <- transform(rankinglrts, percent = n / area)
-
-zoneslrtl <- st_intersection(lrtlines,zones)
-rankinglrtl <- tapply(st_length(zoneslrtl), zoneslrtl$Elementary,sum)
-rankinglrtl <- as.data.frame(rankinglrtl)
-rankinglrtl[is.na(rankinglrtl)] <- "0"
-names(rankinglrtl)[names(rankinglrtl) == "rankinglrtl"] <- "length"
-rankinglrtl <- data.frame(rankinglrtl$length, zones$Elementary, zones$Shape_Area)
-rankinglrtl$rankinglrtl.length <- as.numeric(rankinglrtl$rankinglrtl.length)
-rankinglrtl <- transform(rankinglrtl, percent = rankinglrtl.length / zones.Shape_Area)
-
-zonesbus <- st_join(busstops, zones)
-rankingbus <- tabyl(zonesbus, Elementary)
-rankingbus <- na.omit(rankingbus)
-rankingbus$area <- zones$Shape_Area
-rankingbus <- transform(rankingbus, percentbus = n / area)
-
-zonesbusr <- st_intersection(busroutes, zones)
-rankingbusr <- tapply(st_length(zonesbusr), zonesbusr$Elementary, sum)
-rankingbusr <- as.data.frame(rankingbusr)
-rankingbusr[is.na(rankingbusr)] <- "0"
-names(rankingbusr)[names(rankingbusr) == "rankingbusr"] <- "length"
-rankingbusr <- data.frame(rankingbusr$length, zones$Elementary, zones$Shape_Area)
-rankingbusr$rankingbusr.length <- as.numeric(rankingbusr$rankingbusr.length)
-rankingbusr <- transform(rankingbusr, percent = rankingbusr.length / zones.Shape_Area)
-
-zonesbike <- st_join(bikeways, zones)
-rankingbike <- tapply(st_length(zonesbike), zonesbike$Elementary, sum)
-rankingbike <- as.data.frame(rankingbike)
-rankingbike[is.na(rankingbike)] <- "0"
-names(rankingbike)[names(rankingbike) == "rankingbike"] <- "length"
-rankingbike <- data.frame(rankingbike$length, zones$Elementary, zones$Shape_Area)
-rankingbike$rankingbike.length <- as.numeric(rankingbike$rankingbike.length)
-rankingbike <- transform(rankingbike, percent = rankingbike.length / zones.Shape_Area)
-
-zonessw <- st_join(sidewalks,zones)
-rankingsw <- tapply(st_length(zonessw), zonessw$Elementary,sum)
-rankingsw <- as.data.frame(rankingsw)
-rankingsw[is.na(rankingsw)] <- "0"
-names(rankingsw)[names(rankingsw) == "rankingsw"] <- "length"
-rankingsw <- data.frame(rankingsw$length, zones$Elementary, zones$Shape_Area)
-rankingsw$rankingsw.length <- as.numeric(rankingsw$rankingsw.length)
-rankingsw <- transform(rankingsw, percent = rankingsw.length / zones.Shape_Area)
-
-
-#trying to create 2-mile isochrone map-----
-library(devtools)
-library(osrm)
-
-poe <- osrm::osrmIsometric(
-  loc = c(29.7274389868503, -95.40736370540988),
-  breaks = seq(from = 0.1, to = 3218, length.out = 4),
-  exclude = NULL,
-  res = 30,
-  returnclass = "sf",
-  osrm.server = getOption("osrm.server"),
-  osrm.profile = getOption("foot")
-)
-
+route <- otp_plan(otpcon,
+                  fromPlace = c(-95.3890974,29.7868162),
+                  toPlace = c(-95.40736370540988, 29.7274389868503))
+route
